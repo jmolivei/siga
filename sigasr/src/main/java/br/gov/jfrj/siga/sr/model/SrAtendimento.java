11aaff81d9973bed0cb7cfd60287a216526e7df3
Edson Sales <esredson@gmail.com>
2015-08-24 17:36:26 -0300

    Siga-Sr: melhorias nos SLA's - parte II

--- sigasr/src/main/java/br/gov/jfrj/siga/sr/model/SrAtendimento.java
+++ sigasr/src/main/java/br/gov/jfrj/siga/sr/model/SrAtendimento.java
@@ -7,14 +7,12 @@ import br.gov.jfrj.siga.dp.DpLotacao;
 import br.gov.jfrj.siga.dp.DpPessoa;
 import edu.emory.mathcs.backport.java.util.Arrays;
 
-public class SrAtendimento extends SrEtapaSolicitacao {
-	
-	public SrAtendimento(SrSolicitacao sol, Date dtIni, Date dtFim, DpPessoa responsavel, DpLotacao lotaResponsavel) {
-		super(sol);
-		setInicio(dtIni);
-		setFimEfetivo(dtFim);
-		setResponsavel(responsavel);
-		setLotaResponsavel(lotaResponsavel);
-		setDescricao("Atendimento (" + getLotaResponsavel().getSiglaCompleta() + ")");
+public class SrAtendimento extends SrEtapa {
+
+	@Override
+	public String getDescricao() {
+		return "Atendimento (" + getLotaResponsavel().getSiglaCompleta() + ")";
 	}
+	
+	
 }
